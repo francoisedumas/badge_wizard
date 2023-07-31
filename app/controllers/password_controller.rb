a7679e52ff3d64d997f8a139544088d0cc7dd3a9
Francois Dumas Lattaque <dumaslattaque.francois@gmail.com>
2023-07-31 05:50:22 +0200

    Feat - Cleaning and simplification

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -1,29 +1,27 @@
 # frozen_string_literal: true
 
-# module Profile
-  class PasswordController < ApplicationController
-    before_action :set_user, only: %i[edit update]
+class PasswordController < ApplicationController
+  before_action :set_user, only: %i[edit update]
 
-    def edit
-    end
+  def edit
+  end
 
-    def update
-      if @user.update_with_password(password_params)
-        bypass_sign_in @user
-        redirect_to edit_password_path, notice: t(".update_success")
-      else
-        render :edit, status: :unprocessable_entity, alert: t(".invalid_password")
-      end
-    end
-
-    private
-
-    def password_params
-      params.require(:user).permit(:current_password, :password, :password_confirmation)
-    end
-
-    def set_user
-      @user = current_user
+  def update
+    if @user.update_with_password(password_params)
+      bypass_sign_in @user
+      redirect_to edit_password_path, notice: t(".update_success")
+    else
+      render :edit, status: :unprocessable_entity, alert: t(".invalid_password")
     end
   end
-# end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = current_user
+  end
+end
